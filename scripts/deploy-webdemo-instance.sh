36d75ab6e22ab6a3fee347488587ff2ba3d55de2
Rob Canning <rc@kiben.net>
2025-09-03 10:50:24 +0200

    added path update to ../server.js

--- scripts/deploy-webdemo-instance.sh
+++ scripts/deploy-webdemo-instance.sh
@@ -1,2 +1,3 @@
-~/.local/node_modules/pm2/bin/pm2 start server.js --name "oscillaScore" -- \
+~/.local/node_modules/pm2/bin/pm2 stop oscillaScore
+~/.local/node_modules/pm2/bin/pm2 start ../server.js --name "oscillaScore" -- \
     --port=4899 --osc-in=57121 --osc-out=57121
