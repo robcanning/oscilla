92c500499ae80b45ae5f37737bc8b76db2c03735
Rob Canning <rc@kiben.net>
2026-01-06 02:19:46 +0100

    upgrade to docs system

--- public/docs/.eleventy.js
+++ public/docs/.eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function (eleventyConfig) {
 
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
   return {
     dir: {
       input: "src",
       output: "site",
       includes: "_includes",
-      layouts: "_includes"
+      layouts: "_includes",
+      pathPrefix: "/oscilla"
     },
 
     markdownTemplateEngine: "njk",
